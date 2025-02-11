e8093322327957caa09914ce6d55de60e95292ec
gmpa-mrt <gaetanmeurant@gmail.com>
2025-02-11 22:27:00 +0100

    feat(projects): implement migration

---
+++ database/migrations/1739307642532_create_projects_table.ts
@@ -0,0 +1,33 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'projects'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('project_name', 100).notNullable()
+      table.text('project_description').nullable()
+      table.boolean('is_public').nullable().defaultTo(false)
+      table.boolean('is_archived').nullable().defaultTo(false)
+      // Relation
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.integer('group_id').unsigned().references('groups.id').onDelete('CASCADE')
+
+      // Add a CHECK constraint to enforce that a project
+      // belongs either to a user OR a group, but not both
+      table.check(
+        '(user_id IS NOT NULL AND group_id IS NULL) OR (user_id IS NULL AND group_id IS NOT NULL)'
+      )
+
+      // Date time
+      table.timestamp('created_at')
+      table.timestamp('created_at')
+      table.timestamp('archived_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
